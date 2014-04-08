5a0a45acf267844b81efe379fdf8e564236ccfb9
Lonewolf <nikhilkalige@gmail.com>
2014-04-09 02:18:14 +0530

    parse_var and get-type functions added Test cases for above functions

--- index.js
+++ index.js
@@ -9,16 +9,17 @@
 
 function DocsParser(settings) {
     this.settings = settings;
-    this.setup_settings()
-    this.name_override = None
-}
-
+    //this.setup_settings()
+    //this.name_override = None
+};
+/*
 DocsParser.prototype.is_existing_comment = function(line) {
     return line.search('^\\s*\\*')
 };
-
+*/
 function JsParser() {
-
+    this.name = "nik";
+    this.setup_settings();
 }
 
 
@@ -28,83 +29,88 @@ JsParser.prototype.setup_settings = function() {
         // curly brackets around the type information
         'curlyTypes': true,
         'typeInfo': true,
-        'typeTag': self.viewSettings.get('jsdocs_override_js_var') or 'type',
+        'typeTag': 'type',
         // technically, they can contain all sorts of unicode, but w/e
         'varIdentifier': identifier,
         'fnIdentifier':  identifier,
-        'fnOpener': r'function(?:\s+' + identifier + r')?\s*\(',
+        'fnOpener': /function(?:\s+' + identifier + r')?\s*\(/,
         'commentCloser': ' */',
         'bool': 'Boolean',
         'function': 'Function'
-    }
+    };
 };
 
 JsParser.prototype.parse_function = function(line) {
-    regex =  new RegExp(
+    // (?:([a-zA-Z_$][a-zA-Z_$0-9]*)\\s*[:=]\\s*)?function(?:\\s+([a-zA-Z_$][a-zA-Z_$0-9]*))?\\s*\\(\\s*(.*)\\)
+    var regex =  new RegExp(
         //   fnName = function,  fnName : function
-        '(?:(' + this.settings['varIdentifier'] + ')\\s*[:=]\\s*)?'
-        + 'function'
+        '(?:(' + this.settings.varIdentifier + ')\\s*[:=]\\s*)?' +
+        'function' +
         // function fnName
-        + '(?:\\s+(' + this.settings['fnIdentifier'] + '))?'
+        '(?:\\s+(' + this.settings.fnIdentifier + '))?' +
         // (arg1, arg2)
-        + '\\s*\\(\\s*(.*)\\)'
-    )
+        '\\s*\\(\\s*(.*)\\)'
+    );
 
-    var matches = regex.exec(line)
+    var matches = regex.exec(line);
     if(matches === null) {
-        return null
+        return null;
     }
     for(var i=0;i < matches.length; i++) {
-        console.log(i+ ':' + matches[i])
+        console.log(i+ ':' + matches[i]);
     }
     // grab the name out of "name1 = function name2(foo)" preferring name1
     var name = matches[1] || matches[2] || '';
     var args = matches[3];
-    console.log(name)
-    return [name, args, null]
-}
+    console.log(name);
+    return [name, args, null];
+};
 
-JsParser.prototype.parse_var = function(self, line) {
-    res = re.search(
-        #   var foo = blah,
-        #       foo = blah;
-        #   baz.foo = blah;
-        #   baz = {
-        #        foo : blah
-        #   }
-
-        '(?P<name>' + self.settings['varIdentifier'] + ')\s*[=:]\s*(?P<val>.*?)(?:[;,]|$)',
-        line
-    )
-    if not res:
-        return None
-
-    return (res.group('name'), res.group('val').strip())
-}
+JsParser.prototype.parse_var = function(line) {
+        //   var foo = blah,
+        //       foo = blah;
+        //   baz.foo = blah;
+        //   baz = {
+        //        foo : blah
+        //   }
+    var regex = new RegExp('(' + this.settings.varIdentifier + ')\\s*[=:]\\s*(.*?)(?:[;,]|$)');
+    var matches = regex.exec(line);
+    if(matches === null) {
+        return null;
+    }
+    // variable name, variable value
+    return [matches[1], matches[2].trim()];
+};
 
 JsParser.prototype.guess_type_from_value = function(val) {
-    lowerPrimitives = self.viewSettings.get('jsdocs_lower_case_primitives') or False
-    shortPrimitives = self.viewSettings.get('jsdocs_short_primitives') or False
-    if is_numeric(val):
-    return "number" if lowerPrimitives else "Number"
-    if val[0] == '"' or val[0] == "'":
-    return "string" if lowerPrimitives else "String"
-    if val[0] == '[':
-    return "Array"
-    if val[0] == '{':
-    return "Object"
-    if val == 'true' or val == 'false':
-    returnVal = 'Bool' if shortPrimitives else 'Boolean'
-    return returnVal.lower() if lowerPrimitives else returnVal
-    if re.match('RegExp\\b|\\/[^\\/]', val):
-    return 'RegExp'
-    if val[:4] == 'new ':
-    res = re.search('new (' + self.settings['fnIdentifier'] + ')', val)
-    return res and res.group(1) or None
-    return None
-}
+    //lowerPrimitives = self.viewSettings.get('jsdocs_lower_case_primitives') or False
+    //shortPrimitives = self.viewSettings.get('jsdocs_short_primitives') or False
+    var var_type = typeof(val);
+    if((var_type === 'string') || (var_type === 'number')) {
+        return var_type;
+    }
+    if(val[0] == '[') {
+        return 'Array';
+    }
+    if(val[0] == '{') {
+        return 'Object';
+    }
+    if((val === true) || (val === false)) {
+        return 'Boolean';
+    }
+    var regex = new RegExp('RegExp\\b|\\\/[^\\/]');
+    if(regex.test(val)) {
+        return 'RegExp';
+    }
+    if(val.slice(0, 4) == 'new ') {
+        var regex = new RegExp('new (' + this.settings.fnIdentifier + ')');
+        var matches = regex.exec(val);
+        return (matches[0] && matches[1]) || null;
+    }
+    return null;
+};
 
 
 
 
-module.exports.pr = parse_function
+module.exports.parser = new JsParser();
---
+++ test.js
@@ -0,0 +1,16 @@
+var js = require('./index');
+
+var fu = 'function foobar(var, dat) { }';
+var x = js.parser.parse_function(fu);
+console.log(x);
+
+var fu = 'function someLongFunctionName(withArguments, across,many,lines) { ';
+var x = js.parser.parse_function(fu);
+console.log(x);
+
+
+var fu = 'var asdfff = "asddd"';
+var x = js.parser.parse_var(fu);
+console.log(x);
+
+
